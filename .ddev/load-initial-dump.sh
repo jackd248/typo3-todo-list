89db50be61d6b107bf231906f873b49713421084
Konrad Michalik <konrad.michalik@xima.de>
2024-10-27 19:43:55 +0100

    build: finish initial setup

--- .ddev/load-initial-dump.sh
+++ .ddev/load-initial-dump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the database is empty
-if test -z $(echo "SELECT * FROM be_users" | vendor/bin/typo3 database:import)
+if [ -z "$(echo 'SELECT * FROM pages' | vendor/bin/typo3 database:import)" ]
 then
   echo 'Loading initial database dump'
-  mysql db < /var/www/html/.ddev/dump.sql
+  mariadb db < /var/www/html/.ddev/dump.sql
 fi
---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+defined('TYPO3') or die();
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = '.*';
